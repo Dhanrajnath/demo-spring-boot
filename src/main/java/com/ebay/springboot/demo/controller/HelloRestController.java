cb3322d06296400bc18a70c8b805f8e6e735eb14
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-21 15:06:11 +0530

    props values

--- src/main/java/com/ebay/springboot/demo/controller/HelloRestController.java
+++ src/main/java/com/ebay/springboot/demo/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.ebay.springboot.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import java.time.LocalDateTime;
 @RestController
 public class HelloRestController {
 
+    @Value("${some.envvar}")
+    private String envVar;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!, Time on the server is : " + LocalDateTime.now();
+        return "Hello World!, Time on the server is : " + LocalDateTime.now() + "-->" + envVar;
     }
 }
