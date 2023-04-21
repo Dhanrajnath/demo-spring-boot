bf51f0b61c6cea688932153cede0a8f5f6c7c829
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-21 18:02:21 +0530

    DAO: find,save&delete

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -37,4 +37,40 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
         //return the result
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // get the employee
+        Employee theEmployee =
+                currentSession.get(Employee.class, theId);
+
+        // return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // save employee
+        currentSession.saveOrUpdate(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theEmployeeId) {
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // delete object with primary key
+        Query theQuery =
+                currentSession.createQuery(
+                        "delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId", theEmployeeId);
+
+        theQuery.executeUpdate();
+    }
 }
